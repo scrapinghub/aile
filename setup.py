a70dbef03319293554229d479767eb2268c6bae2
Pedro Lopez-Adeva <plopezadeva@gmail.com>
2015-10-20 16:14:05 +0200

    Restructured directories

--- setup.py
+++ setup.py
@@ -4,14 +4,26 @@ from Cython.Build import cythonize
 
 extra_compile_args = ["-O3"]
 extensions = [
-    Extension("phmm", ["phmm.pyx"],
+    Extension("phmm", ["aile/phmm.pyx"],
               extra_compile_args=extra_compile_args
         ),
-    Extension("hmm", ["hmm.pyx"],
+    Extension("hmm", ["aile/hmm.pyx"],
               extra_compile_args=extra_compile_args
         ),
 ]
 
 setup(
+    name = 'AILE',
+    version = '0.0.1',
+    packages = ['aile'],
+    install_requires = [
+        'numpy',
+        'scipy',
+        'scrapely',
+        'tabulate',
+        'selenium',
+        'pomegranate',
+        'cython',
+        'pandas'],
     ext_modules = cythonize(extensions)
 )
