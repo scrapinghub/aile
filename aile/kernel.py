3c22899ebdba2c09d368f061dfd30fe53ba18c2a
Pedro Lopez-Adeva <plopezadeva@gmail.com>
2015-11-13 12:59:32 +0100

    Cleaned up code. Better speed as a result

--- aile/kernel.py
+++ aile/kernel.py
@@ -9,14 +9,6 @@ import sklearn.cluster
 import aile.page_extractor as pe
 
 
-def filter_empty_text(page):
-    """Return a list of fragments from page where empty text has been deleted"""
-    return [fragment
-            for fragment in page.parsed_body
-            if (not fragment.is_text_content or
-                page.body[fragment.start:fragment.end].strip())]
-
-
 def jaccard_index(s1, s2, null_val=1.0):
     """Compute Jaccard index between two sets"""
     if s1 or s2:
@@ -34,65 +26,126 @@ def is_tag(fragment):
 def get_class(fragment):
     """Return a set with class attributes for a given fragment"""
     if is_tag(fragment):
-        s = set((fragment.attributes.get('class') or '').split())
-        s.add(fragment.tag)
-        return s
+        return set((fragment.attributes.get('class') or '').split())
     else:
-        s = set()
-        if fragment.is_text_content:
-            s.add('[T]')
-        return s
+        return set()
 
 
-def class_similarity(f1, f2, no_class=1.0):
-    return jaccard_index(get_class(f1), get_class(f2), no_class)
-
-
-def match_class(f1, f2, threshold=0.5):
-    """Check if two fragments agree on their class attributes"""
-    return class_similarity >= threshold
-
-
-def tag_equal(f1, f2):
-    """Check if two tags are equal"""
-    return (f1.tag_type == f2.tag_type and f1.tag == f2.tag and
-            match_class(f1, f2))
-
-
-def fragment_equal(f1, f2):
-    """Check if two fragments are equal"""
-    return ((is_tag(f1) and is_tag(f2) and tag_equal(f1, f2)) or
-            (f1.is_text_content and f2.is_text_content))
-
-
-def fragment_to_node(fragment):
-    """Convert a fragment to a node inside a tree where we are going
-    to compute the kernel"""
+def get_tag(fragment):
     if fragment.is_text_content:
         return '[T]'
-    elif (isinstance(fragment, hp.HtmlTag) and
-          fragment.tag_type != hp.HtmlTagType.CLOSE_TAG):
+    elif is_tag(fragment):
         return fragment.tag
+    else:
+        return None
+
+
+class TreeNode(object):
+    __slots__ = ('tag', 'class_attr')
+
+    def __init__(self, tag, class_attr=set()):
+        self.tag = tag
+        self.class_attr = class_attr
+
+    def __hash__(self):
+        return hash(self.tag)
+
+    def __eq__(self, other):
+        return self.tag == other.tag
+
+    def __str__(self):
+        s = self.tag
+        if self.class_attr:
+            s += '['
+            s += ','.join(self.class_attr)
+            s += ']'
+        return s
+
+    def __repr__(self):
+        return self.__str__()
+
+    @classmethod
+    def similarity(cls, a, b, no_class=1.0):
+        return jaccard_index(
+            set([a.tag]) | a.class_attr,
+            set([b.tag]) | b.class_attr,
+            no_class)
+
+
+def non_empty_text(page, fragment):
+    return fragment.is_text_content and\
+        page.body[fragment.start:fragment.end].strip()
+
+
+def fragment_to_node(page, fragment):
+    """Convert a fragment to a node inside a tree where we are going
+    to compute the kernel"""
+    if non_empty_text(page, fragment):
+        return TreeNode('[T]')
+    elif (is_tag(fragment) and
+          fragment.tag_type != hp.HtmlTagType.CLOSE_TAG):
+        return TreeNode(fragment.tag, get_class(fragment))
     return None
 
 
-def get_nodes(fragments):
-    for i, fragment in enumerate(fragments):
-        node = fragment_to_node(fragment)
-        if node:
+def tree_nodes(page):
+    """Return a list of fragments from page where empty text has been deleted"""
+    for i, fragment in enumerate(page.parsed_body):
+        node = fragment_to_node(page, fragment)
+        if node is not None:
             yield (i, node)
 
 
-def order_pairs(fragments):
+class PageTree(object):
+    def __init__(self, page):
+        self.page = page
+        index, self.nodes = zip(*tree_nodes(page))
+        self.index = np.array(index)
+        reverse_index = np.repeat(-1, len(page.parsed_body))
+        for i, idx in enumerate(self.index):
+            reverse_index[idx] = i
+        match = pe.match_fragments(page.parsed_body)
+        self.match = np.repeat(-1, len(self.index))
+        self.parents = np.repeat(-1, len(self.index))
+        for i, m in enumerate(match):
+            j = reverse_index[i]
+            if j >= 0:
+                if m >= 0:
+                    k = -1
+                    while k < 0:
+                        k = reverse_index[m]
+                        m += 1
+                        if m == len(match):
+                            k = len(self.match)
+                            break
+                    assert k >= 0
+                else:
+                    k = j # no children
+                self.match[j] = k
+        for i, m in enumerate(self.match):
+            self.parents[i+1:m] = i
+
+
+    def __len__(self):
+        return len(self.index)
+
+    def children(self, i):
+        return i + 1 + np.flatnonzero(self.parents[i+1:self.match[i]] == i)
+
+    def similarity(self, i, j):
+        return TreeNode.similarity(self.nodes[i], self.nodes[j])
+
+
+def order_pairs(nodes):
     """Given a list of fragments return pairs of equal nodes ordered in
     such a way that if pair_1 comes before pair_2 then no element of pair_2 is
     a descendant of pair_1"""
     grouped = collections.defaultdict(list)
-    for i, name in get_nodes(fragments):
-        grouped[name].append(i)
+    for i, node in enumerate(nodes):
+        grouped[node].append(i)
     return sorted(
         [pair
-         for name, indices in grouped.iteritems()
+         for node, indices in grouped.iteritems()
          for pair in itertools.combinations_with_replacement(sorted(indices), 2)],
         key=lambda x: x[0], reverse=True)
 
@@ -109,31 +162,19 @@ def check_order(op, parents):
         C[i, j] = 1
 
 
-def children(fragments, match, parents, i):
-    """Children of the i-th fragment"""
-    return [k for k in [i + 1 + j for j, fragment in get_nodes(fragments[i+1: max(match[i], i+1)])]
-            if parents[k] == i]
-
-
-def is_ascendant(match, i_child, i_ascendant):
-    """True if the second fragment is an ascendant of the first"""
-    return i_child >= i_ascendant and i_child <= match[i_ascendant]
-
-
-def build_counts(fragments, match, parents,
-                 max_depth=4, sim=class_similarity, max_childs=20):
-    N = len(fragments)
+def build_counts(ptree, max_depth=4, max_childs=20):
+    N = len(ptree)
     if max_childs is None:
         max_childs = N
-    pairs = order_pairs(fragments)
+    pairs = order_pairs(ptree.nodes)
     C = np.zeros((N, N, max_depth), dtype=float)
     S = np.zeros((max_childs + 1, max_childs + 1, max_depth), dtype=float)
     S[0, :, :] = S[:, 0, :] = 1
     for i1, i2 in pairs:
-        ch1 = children(fragments, match, parents, i1)
-        ch2 = children(fragments, match, parents, i2)
-        if not ch1 and not ch2:
-            C[i2, i1, :] = C[i1, i2, :] = sim(fragments[i1], fragments[i2])
+        ch1 = ptree.children(i1)
+        ch2 = ptree.children(i2)
+        if len(ch1) == 0 and len(ch2) == 0:
+            C[i2, i1, :] = C[i1, i2, :] = ptree.similarity(i1, i2)
         else:
             nc1 = min(len(ch1), max_childs)
             nc2 = min(len(ch2), max_childs)
@@ -144,43 +185,36 @@ def build_counts(fragments, match, parents,
                                      S[j1 - 1, j2 - 1, :  ]
                     S[j1, j2, 1:] += S[j1 - 1, j2 - 1, :-1]*C[ch1[j1 - 1], ch2[j2 - 1], :-1]
             C[i2, i1, :] = C[i1, i2, :] = \
-                        sim(fragments[i1], fragments[i2])*S[nc1, nc2, :]
+                        ptree.similarity(i1, i2)*S[nc1, nc2, :]
     return C[:, :, max_depth - 1]
 
 
-def kernel(fragments, match=None, parents=None, counts=None,
-           max_depth=2, sim=class_similarity, max_childs=20, decay=0.1):
-    if match is None:
-        match = pe.match_fragments(fragments)
-    if parents is None:
-        parents = pe.build_tree(match)
+def kernel(ptree, counts=None, max_depth=2, max_childs=20, decay=0.1):
     if counts is None:
-        C = build_counts(fragments, match, parents, max_depth, sim, max_childs)
+        C = build_counts(ptree, max_depth, max_childs)
     else:
         C = counts
     K = np.zeros(C.shape)
     N = K.shape[0]
-
     A = C.copy()
     B = C.copy()
     for i in range(N - 1, -1, -1):
-        pi = parents[i]
+        pi = ptree.parents[i]
         for j in range(N - 1, -1, -1):
-            pj = parents[j]
+            pj = ptree.parents[j]
             if pi > 0:
                 A[pi, j] += decay*A[i, j]
             if pj > 0:
                 B[i, pj] += decay*B[i, j]
     for i in range(N - 1, -1, -1):
-        pi = parents[i]
+        pi = ptree.parents[i]
         for j in range(N - 1, -1, -1):
-            ri = max(match[i], i) + 1
-            rj = max(match[j], j) + 1
+            ri = max(ptree.match[i], i)
+            rj = max(ptree.match[j], j)
             K[i, j] += A[i, j] + B[i, j] - C[i, j]
-            pj = parents[j]
+            pj = ptree.parents[j]
             if pi > 0 and pj > 0:
                 K[pi, pj] += decay*K[i, j]
-
     return K
 
 
@@ -207,42 +241,30 @@ def kernel_to_radial_distance(K):
     return -np.log(normalize_kernel(K))
 
 
-def cluster(fragments, match, parents, K):
+def cluster(K):
     D = kernel_to_distance(normalize_kernel(K))
     clt = sklearn.cluster.DBSCAN(eps=0.76, min_samples=8, metric='precomputed')
-    lab = clt.fit_predict(D)
+    return clt.fit_predict(D)
+
+
+def score_clusters(ptree, labels):
     grp = collections.defaultdict(list)
-    for i, l in enumerate(lab):
+    for i, l in enumerate(labels):
         grp[l].append(i)
     grp = {k: np.array(v) for k, v in grp.iteritems()}
-    scores = {k: sum(max(0, match[i] - i) for i in v)
+    scores = {k: sum(max(0, ptree.match[i] - i + 1) for i in v)
               for k, v in grp.iteritems()}
-    return lab, grp, scores
+    return grp, scores
 
 
-def fragment_name(fragment):
-    if fragment.is_text_content:
-        return '[T]'
-    elif isinstance(fragment, hp.HtmlTag):
-        class_attr = fragment.attributes.get('class')
-        if class_attr:
-            return fragment.tag + '[' + class_attr + ']'
-        else:
-            return fragment.tag
-    else:
-        return ''
-
-
-def build_tree(fragments, parents, labels=None):
+def build_tree(ptree, labels=None):
     root = ete2.Tree(name='root')
-    T = [ete2.Tree(name=(fragment_name(f)+'(' + str(i) + ')'))
-         for i, f in enumerate(fragments)]
+    T = [ete2.Tree(name=(str(node) +'(' + str(i) + ')'))
+         for i, node in enumerate(ptree.nodes)]
     if labels is not None:
         for t, lab in zip(T, labels):
-            t.name += 'lab=' + str(lab)
-    for i, (f, p) in enumerate(zip(fragments, parents)):
-        if isinstance(f, hp.HtmlTag) and f.tag_type == hp.HtmlTagType.CLOSE_TAG:
-            continue
+            t.name += '{' + str(lab) + '}'
+    for i, p in enumerate(ptree.parents):
         if p > 0:
             T[p].add_child(T[i])
         else:
@@ -257,10 +279,9 @@ if __name__ == '__main__':
 #    page = hp.url_to_page('http://www.ebay.com/sch/Car-and-Truck-Tires/66471/bn_584423/i.html')
 #    page = hp.url_to_page('https://patchofland.com/investments.html')
     page = hp.url_to_page('http://jobsearch.monster.co.uk/browse/?re=nv_gh_gnl1147_%2F')
-    fragments = filter_empty_text(page)
-    match = pe.match_fragments(fragments)
-    parents = pe.build_tree(match)
+    page_tree = PageTree(page)
 
-    K = kernel(fragments, match, parents)
-    l, c, s = cluster(fragments, match, parents, K)
-    t = build_tree(fragments, parents, labels=l)
+    K = kernel(page_tree)
+    l = cluster(K)
+    g, s = score_clusters(page_tree, l)
+    t = build_tree(page_tree, labels=l)
